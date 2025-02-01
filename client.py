b8bd3dd2803ced203a3ce8cbcf050446bcef1a8a
Alexis <alexis.chavezguzman7@gmail.com>
2025-02-01 14:06:28 -0500

    Updated client after testing
    -Succesfully connected to server

--- client.py
+++ client.py
@@ -3,17 +3,19 @@ import socket
 SERVER_PORT = 7715  # Chosen port number
 
 def connect_to_server ():
+    #Creates a socket that attempts to connect to server
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(("local host", SERVER_PORT))
+    client_socket.connect(("localhost", SERVER_PORT))
     print("Attempting to connect server port", SERVER_PORT)
 
-    if socket.error:
-        print("Unable to connect to server")
-        exit(-1)
+    #if socket.error:
+    #   print("Unable to connect to server")
+    #   exit(-1)
 
+    #Calls to handle interactions to server, once done, socket closes
     handle_interaction(client_socket)
     client_socket.close()
-    print()
+    print("Disconnected from the server port", SERVER_PORT)
 
 
 def handle_interaction (client):
@@ -27,5 +29,4 @@ def handle_interaction (client):
     TODO: Add proper interaction between server and client
     :return:
     """
-    
 connect_to_server()
