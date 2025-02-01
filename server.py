abad6cff8d0f0819840a9220bafba93f390471f1
DauntlessK <133259396+DauntlessK@users.noreply.github.com>
2025-02-01 13:12:47 -0500

    Update server.py
    server loop done

--- server.py
+++ server.py
@@ -1,3 +1,28 @@
 import socket
 
 SERVER_PORT = 7715  # Chosen port number
+
+def run_server():
+    """
+    Creates a loop that runs endlessly, waiting for connections.
+    """
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("localhost", SERVER_PORT))
+    server_socket.listen(1)
+    print("Server is running on port", SERVER_PORT)
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print("Accepted connection from:", client_address)
+        handle_client(client_socket)
+        client_socket.close()
+
+def handle_client(client_socket):
+    """
+    Handles client connections.
+    :param client_socket:
+    :return:
+    """
+    print("TODO")
+
+run_server()
