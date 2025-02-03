0401dbc29f883196bd3ec11d3c455fad31ed32f1
Alexis <alexis.chavezguzman7@gmail.com>
2025-02-03 16:16:40 -0500

    Successful MSG comunication
    -Added succesful msg comunication
    -Added handling for MSGGET
    -Added handling to MSGSTORE

--- client.py
+++ client.py
@@ -20,9 +20,22 @@ def connect_to_server ():
 
 
 def handle_interaction (client):
+    #Determines what the client input is and sends messages and info according to the input
     while True:
-        client_input = input ("Enter Input")
-        if client_input == 1:
+        client_input = input ("Enter Input: ")
+        if client_input == "MSGGET":
+            #The message is sent then it receives a message from server
+            client.send(client_input.encode())
+            message_received = client.recv(1024).decode().strip()
+            print(message_received)
+        elif client_input == "MSGSTORE":
+            #The message is sent then it receives message and sends another message to store
+            client.send(client_input.encode())
+            message_received = client.recv(1024).decode().strip()
+            print(message_received)
+            new_msg = input ("Enter message to store: ")
+            client.send(new_msg.encode())
+        else:
             break
 
     """
